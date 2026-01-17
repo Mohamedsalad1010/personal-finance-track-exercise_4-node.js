f041af06eea4562c5998d9f23a722364364e2f58
mohamed salad <abdikariimsalad55@gmail.com>
2026-01-17 22:41:29 +0300

    adding online link

--- utils/swagger.js
+++ utils/swagger.js
@@ -14,7 +14,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:4000'
+        url:  process.env.NODE_DEV == "development" ? 'http://localhost:4000' : "https://personal-finance-track-exercise-4-node-js.onrender.com"
       }
     ],
     components: {
