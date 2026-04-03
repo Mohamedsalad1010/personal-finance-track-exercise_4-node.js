678b1ed2afa665156a5bb568b1948af8d987c20a
mohamed salad <abdikariimsalad55@gmail.com>
2026-04-03 11:13:42 +0300

    fix error dist index.html

--- backend/index.js
+++ backend/index.js
@@ -42,7 +42,7 @@ if(process.env.NODE_DEV === 'production'){
     const __dirname= path.dirname(fileURLToPath(import.meta.url))
     app.use(express.static(path.join(__dirname , '../frontend/dist')))
     app.get(/.*/ , (req , res)=>{
-        res.sendFile(path.join(__dirname , '..' , 'frontend', 'dist' , 'index'))
+        res.sendFile(path.join(__dirname , '..' , 'frontend', 'dist' , 'index.html'))
     })
 }
 // use errors handles
